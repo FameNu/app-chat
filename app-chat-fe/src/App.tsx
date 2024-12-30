58f145248fa6a5f496aa2fbbed961b6600b5b45d
FameNu <110712106+FameNu@users.noreply.github.com>
2024-12-30 15:57:48 +0700

    FE-Add: user login functionality and display current username

--- app-chat-fe/src/App.tsx
+++ app-chat-fe/src/App.tsx
@@ -1,16 +1,19 @@
 import BoxListMessage from './components/BoxListMessage'
 import Chat from './models/Chat'
 import { setCookie, getCookie, deleteCookie } from './services/cookie'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 function App() {
   const getChats: string | null = getCookie('chats')
   const loadChats: Chat[] = getChats ? JSON.parse(getChats) : []
 
+  const getUser: string | null = getCookie('user') || ''
+
   const [chats, setChats] = useState<Chat[]>(loadChats)
 
   const [message, setMessage] = useState<string>('')
   const [username, setUsername] = useState<string>('')
+  const [currentUser, setCurrentUser] = useState<string>(getUser)
 
   const updateUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value)
@@ -46,20 +49,40 @@ function App() {
     console.log(getCookie('chats'))
   }
 
+  const updateUser = () => {
+    setCurrentUser(username)
+    setCookie('user', username)
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleEventSubmitForm = (fnAggrement: Function, e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    fnAggrement()
+  }
+
   return (
     <div className="flex flex-col gap-4 h-full w-10/12 mx-auto">
       <h1 className="text-2xl font-bold text-center my-6">Welcome to u and me Chat Group</h1>
-      <label className="form-control w-full max-w-sm">
-        <div className="label">
-          <span className="label-text">Enter your username</span>
-        </div>
-        <input
-          type="text"
-          className="input input-bordered"
-          placeholder="Type Here"
-          onChange={updateUsername}
-        />
-      </label>
+      <h2>{currentUser}</h2>
+      <form
+        className="flex items-end gap-4 max-w-sm"
+        onSubmit={(e) => handleEventSubmitForm(updateUser, e)}
+      >
+        <label className="form-control w-full max-w-sm">
+          <div className="label">
+            <span className="label-text">Enter your username</span>
+          </div>
+          <input
+            type="text"
+            className="input input-bordered"
+            placeholder="Type Here"
+            onChange={updateUsername}
+          />
+        </label>
+        <button className="btn" type="submit">
+          Login
+        </button>
+      </form>
       <div className="flex items-end gap-4">
         <form className="flex-grow flex items-end gap-4 max-w-sm" onSubmit={sendMessage}>
           <label className="form-control w-full">
