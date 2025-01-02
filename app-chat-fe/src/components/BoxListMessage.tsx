ea7e4013b3af54691e525b85601a4fa785b1440e
FameNu <110712106+FameNu@users.noreply.github.com>
2025-01-02 17:29:52 +0700

    FE-Modify: implement auto-scrolling in BoxListMessage component using useRef and useEffect hooks

--- app-chat-fe/src/components/BoxListMessage.tsx
+++ app-chat-fe/src/components/BoxListMessage.tsx
@@ -1,9 +1,18 @@
 import BoxMessage from './BoxMessage'
 import type Chat from '@/models/Chat'
+import { useRef, useEffect } from 'react';
 
 function BoxListMessage({ chats, username }: { chats: Chat[]; username: string }) {
+  const boxlist = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (boxlist.current) {
+      boxlist.current.scrollTop = boxlist.current.scrollHeight
+    }
+  }, [chats])
+
   return (
-    <div className="flex flex-col gap-4 p-16 border rounded-xl h-4/6 overflow-y-auto">
+    <div ref={boxlist} className="flex flex-col gap-4 p-16 border rounded-xl h-4/6 overflow-y-auto">
       {chats.map((chat, index) => (
         <BoxMessage chat={chat} key={index} currentUser={username} />
       ))}
