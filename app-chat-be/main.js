d8375264bac5bf3acb42cb1ad53ffca2dbe0d098
FameNu <110712106+FameNu@users.noreply.github.com>
2025-01-22 17:06:20 +0700

    mockup response data

--- app-chat-be/main.js
+++ app-chat-be/main.js
@@ -4,6 +4,15 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
+const chats = [
+  { username: 'Alice', message: 'Hello' },
+  { username: 'Bob', message: 'Hi' },
+]
+
+app.get('/', (req, res) => {
+  res.json(chats)
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
