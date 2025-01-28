eaf1680be9373d291fbaed757568f5a8d34f4a73
FameNu <110712106+FameNu@users.noreply.github.com>
2025-01-28 11:43:57 +0700

    add cors dependency on BE

--- app-chat-be/src/app.ts
+++ app-chat-be/src/app.ts
@@ -1,7 +1,32 @@
 import express, { Express } from 'express'
+import cors from 'cors'
 import chatRouter from '@/routes/ChatRoute'
 
+const BASE_URL: string | undefined = process.env.BASE_URL
+
 const app: Express = express()
+
+const crossOriginOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    const allowedOrigins = ['http://localhost:5173']
+    if (BASE_URL) allowedOrigins.push(BASE_URL)
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true) // for allowed origins
+    } else {
+      callback(new Error('Not allowed by CORS')) // for not allowed origins
+    }
+  },
+  method: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // Allow cookies or credentials
+  optionsSuccessStatus: 200, // Status for preflight requests (default is 204)
+  // allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}
+
+app.use(cors(crossOriginOptions))
 
 app.use(express.json())
 
